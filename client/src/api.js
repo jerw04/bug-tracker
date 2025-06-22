40880da2a618e7da0a68e7ef85e5f7f32a81f049
jerw04 <138956340+jerw04@users.noreply.github.com>
2025-06-23 00:57:40 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://bug-tracker-1-dukb.onrender.com/api',
 });
 
 api.interceptors.request.use(config => {
